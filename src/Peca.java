c4f12519876d37289ff24fcd2380ce45ef4cf5e1
Bruno Grassi Ferreira <brunoferreira03@globo.com>
2021-09-17 09:35:39 -0300

    Initial commit

---
+++ src/Peca.java
@@ -0,0 +1,28 @@
+public class Peca {
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    private String marca;
+    private String modelo;
+
+    public String toString(){
+        return "Marca: " + marca + ", Modelo: " + modelo;
+    }
+
+//    public abstract void retirar();
+
+}
